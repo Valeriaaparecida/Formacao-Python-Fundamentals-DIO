61aabe6bb3acf87a2d74acd18aa09e3255d8cf1c
Valeria Aparecida <valeria777valeria@hotmail.com>
2025-09-01 21:08:03 -0300

    feat: add fundamentos module files

---
+++ Fundamentos/funcao_entrada_saida.py
@@ -0,0 +1,9 @@
+nome = input("Informe seu nome: ")
+idade = input("Informe sua idade: ")
+print("Olá", nome, "você tem", idade, "anos")
+print(nome, idade)
+
+print("teste", end="...\n ")
+print(nome, idade, end="...\n ")
+print(nome,idade, sep="#", end="...\n ")
+print(nome, idade, sep="#")
