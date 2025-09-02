73de412009421455c0868b15e6f9ce0e895b3b94
Valeria Aparecida <valeria777valeria@hotmail.com>
2025-09-01 22:10:20 -0300

    feat: add fundamentos module files

---
+++ Fundamentos/operadores_atribuicao.py
@@ -0,0 +1,23 @@
+Saldo = 1000
+Deposito = 20
+Saque = 10
+Saldo = 1000
+Saldo += Deposito  
+print(Saldo)
+Saldo -= Saque  
+print(Saldo)
+Saldo *= 2
+print(Saldo)
+Saldo /= 3
+print(Saldo)
+Saldo //= 3
+print(Saldo)
+Saldo **= 2
+print(Saldo)
+Saldo %= 2
+print(Saldo)
+print()
+
+saldo = 1000
+Saldo *= 2
+print(Saldo)
