5a668c4a01acbe2b4edfaa3cccdce891d3dc9779
Valeria Aparecida <valeria777valeria@hotmail.com>
2025-09-02 06:56:56 -0300

    feat: add fundamentos module files

---
+++ Fundamentos/operadores_associacao.py
@@ -0,0 +1,12 @@
+frutas = ["maçã", "banana", "laranja"]
+
+print("maçã" in frutas)   
+print("pera" in frutas)    
+print("uva" not in frutas)
+
+texto = "Python é incrível"
+
+print("Python" in texto)  
+print("java" in texto)     
+print("java" not in texto)
+print("crível" in texto)      
