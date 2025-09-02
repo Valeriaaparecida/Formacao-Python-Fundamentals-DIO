0f255dad4319d4912f327c420df2d3d0703906cf
Valeria Aparecida <valeria777valeria@hotmail.com>
2025-09-01 21:44:12 -0300

    feat: add fundamentos module files

---
+++ Fundamentos/operadores_comparacao.py
@@ -0,0 +1,11 @@
+saldo = 1200
+saque = 374
+
+print(saldo == saque)  
+print(saldo != saque)
+print(saldo > saque)
+print(saldo < saque)
+print(saldo >= saque)
+print(saldo <= saque)
+print(saque <= saque)
+print(saque >= saque)
