b9d0c1b844ec5a94eabad96096e891b45ba54afa
Valeria Aparecida <valeria777valeria@hotmail.com>
2025-09-01 22:15:57 -0300

    feat: add fundamentos module files

---
+++ Fundamentos/operadores_logicos.py
@@ -0,0 +1,33 @@
+print(True and True)     
+print(True and False)     
+print(False and False)    
+
+print(True or True)      
+print(True or False)     
+print(False or False)     
+
+print(not True)           
+print(not False)         
+
+# Misturando
+print(True and not False)    
+print(False or not False)    
+print(not (True and False))  
+
+# Comparações com números
+print(5 > 3 and 10 < 20)     
+print(5 > 3 and 10 > 20)      
+print(5 > 10 or 3 < 1)        
+print(5 > 10 or 3 < 10)      
+print(not (5 == 5))           
+
+# Strings
+print("a" == "a" and "b" == "b")  
+print("a" == "a" or "b" == "c")   
+print(not ("a" == "b"))           
+
+# Com listas
+print(1 in [1, 2, 3] and 4 in [1, 2, 3])   
+print(1 in [1, 2, 3] or 4 in [1, 2, 3])   
+print(not (2 in [1, 2, 3]))                
+print(2 not in [1, 2, 3])                  
