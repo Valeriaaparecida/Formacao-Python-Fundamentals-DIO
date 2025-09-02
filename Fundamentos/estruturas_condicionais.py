9a4ec603edf81c1325a411eca7627ec9308d3532
Valeria Aparecida <valeria777valeria@hotmail.com>
2025-09-02 07:12:36 -0300

    feat: add fundamentos module files

--- Fundamentos/estruturas_condicionais.py
+++ Fundamentos/estruturas_condicionais.py
@@ -54,3 +54,23 @@ elif op == "/":
         print("Erro: divisão por zero")
 else:
     print("Operação inválida")
+
+
+
+numero = int(input("Digite um número: "))
+if numero == 1:
+    print("DOMINGO")
+elif numero == 2:
+    print("SEGUNDA")
+elif numero == 3:
+    print("TERÇA")
+elif numero == 4:
+    print("QUARTA")
+elif numero == 5:
+    print("QUINTA")
+elif numero == 6:
+    print("SEXTA")
+elif numero == 7:
+    print("SÁBADO")
+else:
+    print("Número inválido, digite um número entre 1 e 7.")    
