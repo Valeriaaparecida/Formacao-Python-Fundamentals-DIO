6d07ed3159e3d292094d9a9dae7e073b49009fd4
Valeria Aparecida <valeria777valeria@hotmail.com>
2025-09-07 12:04:59 -0300

    feat: add POO module files

--- Python POO/heranca_simples.py
+++ Python POO/heranca_simples.py
@@ -7,6 +7,9 @@ class Veiculo:
     def ligar_motor(self):
         print("Ligando motor")
 
+    def __str__(self):
+        return f"{self.__class__.__name__}(cor={self.cor}, placa={self.placa}, numero_rodas={self.numero_rodas})"    
+
 class Motocicleta(Veiculo):
     pass
 
@@ -16,13 +19,20 @@ class Carro(Veiculo):
     pass
 
 class Caminhao(Veiculo):
-    pass
-
+    def  __init__(self, cor, placa, numero_rodas, carregado):
+        super().__init__(cor, placa, numero_rodas)
+        self.carregado = carregado
+    
+    def esta_carregado(self):
+        print("O caminhão está carregado" if self.carregado else "O caminhão não está carregado")
+      
+    def __str__(self):
+        return f"{self.__class__.__name__}(cor={self.cor}, placa={self.placa}, numero_rodas={self.numero_rodas}, carregado={self.carregado})"  
 moto = Motocicleta("Vermelha", "ABC-1234", 2)
-moto.ligar_motor()
-
 carro = Carro("Azul", "XYZ-5678", 4)
-carro.ligar_motor()
+caminhao = Caminhao("Branco", "DEF-9012", 6, True)
 
-caminhao = Caminhao("Branco", "DEF-9012", 6)
-caminhao.ligar_motor()
+
+print(moto)
+print(carro)
+print(caminhao)
