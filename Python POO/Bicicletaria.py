4d6eb6e96dbb0f9220de022f88ca6f37c9d6036d
Valeria Aparecida <valeria777valeria@hotmail.com>
2025-09-07 10:35:01 -0300

    feat: add POO module files

--- Python POO/Bicicletaria.py
+++ Python POO/Bicicletaria.py
@@ -17,8 +17,15 @@ class Bicicleta:
     def correr(self):
             print("Vrummmm...")
 
+    def __str__(self):
+         return f"{self.marca} {self.modelo} {self.ano} {self.cor} {self.preco}"
+    def __str__(self):
+         return f"{self.__class__.__name__} : {', '.join(f'{chave}={valor}' for chave, valor in self.__dict__.items())}"
 
-b1 = Bicicleta("Caloi", "Mountain Bike", 2021, "Vermelha", 1500.00)
-b1.buzinar()
-b1.correr()
-b1.parar()
+# b1 = Bicicleta("Caloi", "Mountain Bike", 2021, "Vermelha", 1500.00)
+# b1.buzinar()
+# b1.correr()
+# b1.parar()
+
+b2 = Bicicleta("Monark", "Speed", 2020, "Azul", 1200)
+print(b2)
