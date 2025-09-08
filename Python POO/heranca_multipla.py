db892942b50f1b4c958a41e725051bc255364075
Valeria Aparecida <valeria777valeria@hotmail.com>
2025-09-08 14:22:20 -0300

    feat: add POO module files

--- Python POO/heranca_multipla.py
+++ Python POO/heranca_multipla.py
@@ -2,24 +2,32 @@ class Animal:
     def __init__(self, nro_patas):
         self.nro_patas = nro_patas
 
+    def __str__(self):
+        return f"{self.__class__.__name__} : {','.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"    
+
 class Mamifero(Animal):
-    def __init__(self, nro_patas):
-        self.nro_patas = nro_patas
-        super().__init__(nro_patas)
+    def __init__(self, cor_pelo, **kw):
+        self.cor_pelo = cor_pelo
+        super().__init__(**kw)
 
 class Ave(Animal):
-    def __init__(self, nro_patas):
-        self.nro_patas = nro_patas
-        super().__init__(nro_patas)
+    def __init__(self, cor_bico, **kw):
+      self.cor_bico = cor_bico
+      super().__init__(**kw)
 
-class Cachorro(Mamifero):
-    pass
 
 class Gato(Mamifero):
     pass
 
-class Leao(Mamifero):
-    pass
+
 
 class Ornitorrinco(Mamifero, Ave):
     pass
+
+
+
+# gato = Gato(4, "Preto")
+# print(gato)
+
+onitorrionco = Ornitorrinco(nro_patas=4, cor_pelo="Marrom", cor_bico="Laranja")
+print(onitorrionco)
