c365578c20b7c6c062b90c0ab0534645e19084e2
Valeria Aparecida <valeria777valeria@hotmail.com>
2025-09-09 11:21:59 -0300

    feat: add POO module files

--- Python POO/polimorfismo.py
+++ Python POO/polimorfismo.py
@@ -19,6 +19,13 @@ def plano_voo(obj):
     obj.voar()   
 
 
+
+class Carro:
+    def voar(self):
+        print("Carro voador ativando propulsores!")    
+
+
 plano_voo(Pardal())
 plano_voo(Avestruz())
-plano_voo(Aviao())               
+plano_voo(Aviao())    
+plano_voo(Carro())           
